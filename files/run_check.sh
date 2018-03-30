afd611644f28d03834a7b3b431a7667aba89d43d
josebarn <joseph.barnett@cloudlock.com>
2018-03-30 09:26:44 -0400

    adding ability to ignore non-scored failures

--- files/run_check.sh
+++ files/run_check.sh
@@ -1,8 +1,11 @@
 #/bin/bash
-$1
 ./docker-bench-security.sh -c $1 | grep -qce "WARN"
 not_found=$?
 if [ "$not_found" == "1" ]; then
+    exit 0
+fi
+# Done exit with bad code if not scored
+if [ "$2" == "0"]; then
     exit 0
 fi
 ./docker-bench-security.sh -c $1
